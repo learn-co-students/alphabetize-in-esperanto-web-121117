272afdc3cbe12de6deaf0cbdef4d6611208c8641
krandles <krandles@gmail.com>
2017-11-26 01:39:31 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by! do |item|
+    item.split("").map do |esper|
+      ESPERANTO_ALPHABET.index(item[esper])
+    end
+  end
+  arr
 end
